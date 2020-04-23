e94504675cb7ea93d5b71ed0d2a5a70145c3bf94
Shingo Kitagawa <shingogo@hotmail.co.jp>
2020-04-24 03:17:17 +0900

    update test_start_plugin and test_stop_plugin

--- app_manager_plugins/src/app_manager_plugins/test_start_plugin.py
+++ app_manager_plugins/src/app_manager_plugins/test_start_plugin.py
@@ -3,9 +3,9 @@ import rospy
 
 class TestStartPlugin(object):
     @classmethod
-    def app_manager_start_plugin(cls, ctx=None):
-        rospy.loginfo('Testing start plugin')
+    def app_manager_start_plugin(cls, app):
+        rospy.loginfo('Testing start plugin: {}'.format(app.display_name))
 
     @classmethod
-    def app_manager_stop_plugin(cls, ctx=None):
+    def app_manager_stop_plugin(cls, app):
         pass
--- app_manager_plugins/src/app_manager_plugins/test_stop_plugin.py
+++ app_manager_plugins/src/app_manager_plugins/test_stop_plugin.py
@@ -3,9 +3,9 @@ import rospy
 
 class TestStopPlugin(object):
     @classmethod
-    def app_manager_start_plugin(cls, ctx=None):
+    def app_manager_start_plugin(cls, app):
         pass
 
     @classmethod
-    def app_manager_stop_plugin(cls, ctx=None):
-        rospy.loginfo('Testing stop plugin')
+    def app_manager_stop_plugin(cls, app):
+        rospy.loginfo('Testing stop plugin: {}'.format(app.display_name))
