c803ffc2c89a9235f233b6680a415f01bd9acd4e
Shingo Kitagawa <shingogo@hotmail.co.jp>
2020-04-24 21:20:29 +0900

    use python-setuptools for noetic

--- app_manager_plugins/setup.py
+++ app_manager_plugins/setup.py
@@ -1,9 +1,6 @@
-#!/usr/bin/env python
-
-from distutils.core import setup
-
 from catkin_pkg.python_setup import generate_distutils_setup
 from setuptools import find_packages
+from setuptools import setup
 
 
 d = generate_distutils_setup(
