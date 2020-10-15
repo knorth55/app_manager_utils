7f0c2b066db5b3cc36c22907dbe944b8a2593f31
Shingo Kitagawa <shingogo@hotmail.co.jp>
2020-10-15 17:05:05 +0900

    update test_app

--- test_app_manager/apps/test_app/test_app.py
+++ test_app_manager/apps/test_app/test_app.py
@@ -8,7 +8,11 @@ import rospy
 
 def main():
     rospy.init_node('test_app')
+    param1 = rospy.get_param('~param1')
+    param2 = rospy.get_param('~param2')
     rospy.loginfo('test_app started.')
+    rospy.loginfo('param1: {}'.format(param1))
+    rospy.loginfo('param2: {}'.format(param2))
     time.sleep(10)
     rospy.loginfo('test_app stopped.')
     sys.exit(0)
