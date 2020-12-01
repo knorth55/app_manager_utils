13d0ea176e05229f8c1911c69bc07868a4c95b8f
Shingo Kitagawa <shingogo@hotmail.co.jp>
2020-12-01 19:44:16 +0900

    update __init__.py in app_notifier

--- app_notifier/src/app_notifier/__init__.py
+++ app_notifier/src/app_notifier/__init__.py
@@ -1,4 +1,3 @@
-from app_notifier import util  # NOQA
-
 from app_notifier.mail_notifier_plugin import MailNotifierPlugin  # NOQA
 from app_notifier.speech_notifier_plugin import SpeechNotifierPlugin  # NOQA
+from app_notifier.user_speech_notifier_plugin import UserSpeechNotifierPlugin  # NOQA
