334699c107a0c5ccfcff5c65995075644df0b2f2
Shingo Kitagawa <shingogo@hotmail.co.jp>
2020-12-01 23:40:44 +0900

    add warning in UserSpeechNotifierPlugin

--- app_notifier/src/app_notifier/user_speech_notifier_plugin.py
+++ app_notifier/src/app_notifier/user_speech_notifier_plugin.py
@@ -15,16 +15,25 @@ class UserSpeechNotifierPlugin(AppManagerPlugin):
     @classmethod
     def app_manager_start_plugin(cls, app, ctx, plugin_args):
         client_name = plugin_args['client_name']
+        if 'warning' in plugin_args:
+            warning = plugin_args['warning']
+        else:
+            warning = False
         username = rospy.get_param('/app_manager/running_user_name', None)
-        lang = None
-        if 'lang' in plugin_args:
-            lang = plugin_args['lang']
-        client = actionlib.SimpleActionClient(client_name, SoundRequestAction)
+
+        speech_text = None
         if username:
             speech_text = "{} is starting {} app".format(
                 username, app.display_name)
-        else:
+        elif warning:
             speech_text = "Unknown user is starting {} app".format(
                 app.display_name)
-        speak(client, speech_text, lang=lang)
+
+        if speech_text is not None:
+            lang = None
+            if 'lang' in plugin_args:
+                lang = plugin_args['lang']
+            client = actionlib.SimpleActionClient(
+                client_name, SoundRequestAction)
+            speak(client, speech_text, lang=lang)
         return ctx
