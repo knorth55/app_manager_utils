3100dd4140bfb9c2623b3bf0722c371fb6bfd560
Shingo Kitagawa <shingogo@hotmail.co.jp>
2021-07-29 21:45:57 +0900

    flake8

--- app_notification_saver/src/app_notification_saver/__init__.py
+++ app_notification_saver/src/app_notification_saver/__init__.py
@@ -1,3 +1,2 @@
 from app_notification_saver.app_notification_saver_base import AppNotificationSaver  # NOQA
 from app_notification_saver.service_notification_saver import ServiceNotificationSaver  # NOQA
-
