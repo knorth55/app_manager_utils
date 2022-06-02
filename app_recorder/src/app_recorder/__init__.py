5ce7845d651e3e24dde38a8fb5ddbdf5983919ef
Naoya Yamaguchi <708yamaguchi@gmail.com>
2022-06-02 17:07:58 +0900

    [app_recorder] Add rosbag converter plugins

--- app_recorder/src/app_recorder/__init__.py
+++ app_recorder/src/app_recorder/__init__.py
@@ -3,3 +3,5 @@ from app_recorder.audio_video_recorder_plugin import AudioVideoRecorderPlugin  #
 from app_recorder.result_recorder_plugin import ResultRecorderPlugin  # NOQA
 from app_recorder.rosbag_recorder_plugin import RosbagRecorderPlugin  # NOQA
 from app_recorder.video_recorder_plugin import VideoRecorderPlugin  # NOQA
+from app_recorder.rosbag_audio_converter_plugin import RosbagAudioConverterPlugin  # NOQA
+from app_recorder.rosbag_video_converter_plugin import RosbagVideoConverterPlugin  # NOQA
