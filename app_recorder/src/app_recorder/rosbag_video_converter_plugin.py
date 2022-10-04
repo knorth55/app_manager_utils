3557f7f3a749325c7fa3a60c8b2ab3ec774abb30
Shingo Kitagawa <shingogo@hotmail.co.jp>
2022-10-04 17:42:17 +0900

    skip using bag_to_audio/video in plugin

--- app_recorder/src/app_recorder/rosbag_video_converter_plugin.py
+++ app_recorder/src/app_recorder/rosbag_video_converter_plugin.py
@@ -9,9 +9,11 @@ if distutils.spawn.find_executable('ffmpeg'):
     except ImportError as e:
         rospy.logerr('{}'.format(e))
         rospy.logerr('Skip using rosbag_video_converter_plugin')
+        bag_to_video = None
 else:
     rospy.logerr('ffpmeg is not found.')
     rospy.logerr('Skip using rosbag_video_converter_plugin')
+    bag_to_video = None
 
 from app_manager import AppManagerPlugin
 
@@ -22,22 +24,25 @@ class RosbagVideoConverterPlugin(AppManagerPlugin):
 
     @classmethod
     def app_manager_stop_plugin(cls, app, ctx, plugin_args):
-        rosbag_file_path = os.path.join(
-            str(plugin_args['rosbag_path']),
-            str(plugin_args['rosbag_title']))
-        kwargs = {'output_filepath': str(plugin_args['video_path']),
-                  'image_topic': str(plugin_args['image_topic_name']),
-                  'fps': int(plugin_args['image_fps'])}
-        # If audio_topic_name is given, video with audio is converted
-        if 'audio_topic_name' in plugin_args:
-            audio_kwargs = {
-                'audio_topic': str(plugin_args['audio_topic_name']),
-                'samplerate': int(plugin_args['audio_sample_rate']),
-                'channels': int(plugin_args['audio_channels'])}
-            kwargs.update(audio_kwargs)
-        try:
-            bag_to_video(rosbag_file_path, **kwargs)
-        except ValueError as e:
-            # topic is not included in bagfile
-            rospy.logerr('{}'.format(e))
+        if bag_to_video:
+            rosbag_file_path = os.path.join(
+                str(plugin_args['rosbag_path']),
+                str(plugin_args['rosbag_title']))
+            kwargs = {'output_filepath': str(plugin_args['video_path']),
+                      'image_topic': str(plugin_args['image_topic_name']),
+                      'fps': int(plugin_args['image_fps'])}
+            # If audio_topic_name is given, video with audio is converted
+            if 'audio_topic_name' in plugin_args:
+                audio_kwargs = {
+                    'audio_topic': str(plugin_args['audio_topic_name']),
+                    'samplerate': int(plugin_args['audio_sample_rate']),
+                    'channels': int(plugin_args['audio_channels'])}
+                kwargs.update(audio_kwargs)
+            try:
+                bag_to_video(rosbag_file_path, **kwargs)
+            except ValueError as e:
+                # topic is not included in bagfile
+                rospy.logerr('{}'.format(e))
+        else:
+            rospy.logerr('Skipping rosbag_video_converter_plugin')
         return ctx
